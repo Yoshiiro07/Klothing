0ad18c7245b799c793781d447da385f8a248cbab
Gui Oliveira <59020911+Yoshiiro07@users.noreply.github.com>
2024-07-15 12:36:53 -0300

    Starter App
    Starter App

---
+++ src/main/kotlin/com/moonbolt/Klothing/KlothingApplication.kt
@@ -0,0 +1,11 @@
+package com.moonbolt.Klothing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KlothingApplication
+
+fun main(args: Array<String>) {
+	runApplication<KlothingApplication>(*args)
+}
---
+++ src/test/kotlin/com/moonbolt/Klothing/KlothingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.moonbolt.Klothing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KlothingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
